7e8ead6bba96280c6dd888973695b250d794d6dc
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2017-05-06 20:05:45 +0300

    updated tripFinal.owl

--- src/http/HttpHandlerImpl.java
+++ src/http/HttpHandlerImpl.java
@@ -1,7 +1,14 @@
 package http;
 
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
+import queries.OntologyLink;
 
 @SuppressWarnings("restriction")
 public class HttpHandlerImpl {
@@ -18,4 +25,23 @@ public class HttpHandlerImpl {
 			
 		}
 	};
+	
+	public static HttpHandler querySixteen = new HttpHandler() {
+		@Override
+		public void handle(HttpExchange t) {
+			String r = OntologyLink.getInstance().querySixteen(); 
+			
+			t.sendResponseHeaders(200, result.length());
+			OutputStream os = t.getResponseBody();
+			os.write(result.getBytes());
+			os.close();
+		}
+	};
+	
+	private static void outputResult(HttpExchange t, int status, String result) throws IOException {
+		t.sendResponseHeaders(status, result.length());
+		OutputStream os = t.getResponseBody();
+		os.write(result.getBytes());
+		os.close();
+	}
 }
