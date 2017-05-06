1638fbbfc2386bbc0aaf84a7c0417f5f414c1d7a
Tarek ElBeih <tiitomoha@gmail.com>
2017-05-06 18:14:13 +0200

    made ontology link an instance

--- src/queries/OntologyLink.java
+++ src/queries/OntologyLink.java
@@ -8,49 +8,58 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.RDFNode;
 
 public class OntologyLink {
+
 	private Model model;
 	String prefix = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
 			+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>" + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
 			+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
 			+ "PREFIX trip: <http://www.semanticweb.org/tripAdvisor/Trip#>";
-
-	public void createModel() {
+	
+	private static OntologyLink instance;
+	
+	private OntologyLink(){
 		model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
 		model.read("tripFinal.owl");
 	}
+	
+	public static synchronized OntologyLink getInstance() {
+		if(instance == null) {
+			instance = new OntologyLink();
+		}
+		
+		return instance;
+	}
 
-	public void bestSeason(String season) {
+	public String bestSeason(String season) {
 		String query = prefix + "SELECT ?city" + "\n" + "WHERE { ?city trip:bestSeason ?season." + "FILTER regex(?season, "
 				+ '"' + season + '"' + ")}";
-		executeQuery(query, "city");
+		return executeQuery(query, "city");
 	}
 
-	public void hasCategory(String cat) {
+	public String hasCategory(String cat) {
 		String query = prefix + "SELECT ?subject" + "WHERE {?subject trip:hasCategory trip:" + cat + " }";
-		executeQuery(query, "ativity");
+		return executeQuery(query, "ativity");
 	}
 
-	public void includes(String cat) {
+	public String includes(String cat) {
 		String query = prefix + "SELECT *" + "WHERE {" + "?city rdf:type trip:City.\n"
 				+ "?city trip:includes ?activity.\n" + "?activity trip:hasCategory trip:" + cat + "}";
-		executeQuery(query, "city");
+		return executeQuery(query, "city");
 	}
 
-	public void orderCatbyRank(String cat) {
+	public String orderCatbyRank(String cat) {
 		String query = prefix + "SELECT ?activity ?rank" + "WHERE { ?activity trip:hasCategory trip:" + cat + "."
 				+ "?review trip:rated ?activity." + "?review trip:rate ?rank }";
-		executeQuery(query, "activity");
+		return executeQuery(query, "activity");
 	}
 
-	public void executeQuery(String querystr, String wanted) {
+	public String executeQuery(String querystr, String wanted) {
 		Query query = QueryFactory.create(querystr);
 		QueryExecution qexec = QueryExecutionFactory.create(query, model);
 
@@ -60,17 +69,10 @@ public class OntologyLink {
 		ResultSetFormatter.outputAsJSON(outputStream, results);
 		String json = new String(outputStream.toByteArray());
 
-		System.out.println(json);
-		while (results.hasNext()) {
-			QuerySolution sol = results.nextSolution();
-			RDFNode items = sol.get(wanted);
-			System.out.println(items);
-		}
+		return json;
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		OntologyLink x = new OntologyLink();
-		x.createModel();
-		x.bestSeason("Winter");
+		System.out.println(OntologyLink.getInstance().bestSeason("Winter"));
 	}
 }
